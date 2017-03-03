33e50be11990d5dd7864b642391201b2d641349e
Nishant Greene <nishant@nrgmultimedia.com>
2017-03-03 14:14:27 -0800

    Update polycreate-rename.py

--- python/polycreate-rename.py
+++ python/polycreate-rename.py
@@ -1,3 +1,6 @@
 import maya.cmds as nrg
-nrg.polySphere();
+nrg.polySphere(r=1, sx=40, sy=20, ax=(2, 1, 2), cuv=2, ch=1);
+
+nrg.setAttr ("polySphere1.radius", 10);
+
 nrg.rename("pSphere1","nrgSphere");

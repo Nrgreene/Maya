fd2b7374d6a5f2517bfc9677eaa75e9db0c6f0ef
Nishant Greene <nishant@nrgmultimedia.com>
2017-03-03 19:01:59 -0800

    Update parent-allignment.py

--- python/parent-allignment.py
+++ python/parent-allignment.py
@@ -1,6 +1,14 @@
 #Object allignment using parent constraints
+import maya.cmds as nrg
 
-#Create a parentConstraint without the offset
-prtCns = nrg.parentConstraint()
-#The parentConstraint is then removed
-nrg.delete(prtCns)
+objAligner = nrg.window(title="Object Allignment Tool", s=False, wh=(300,100))
+nrg.columnLayout(adj=True)
+nrg.text(l="select the source, then target")
+nrg.button(l="Alignment", w=300, h=100, c="aligner()")
+nrg.showWindow(objAligner)
+
+def aligner():
+  #Create a parentConstraint without the offset
+  prtCns = nrg.parentConstraint()
+  #The parentConstraint is then removed
+  nrg.delete(prtCns)
